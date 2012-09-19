2c89ff80fe898cc368b7abb48fee240008fc88a3
Matt Reid <matt@nomadic-recording.com>
2012-09-19 17:11:15 +0000

    ***

--- wowzaparser.py
+++ wowzaparser.py
@@ -3,6 +3,7 @@ import datetime
 from Bases import logfileparser
 
 class WowzaEntry(logfileparser.DelimitedLogEntry):
+    _datetime_fmt_str = '<datetime>%Y-%y-%d %H:%M:%S.%f'
     def __init__(self, **kwargs):
         super(WowzaEntry, self).__init__(**kwargs)
         if None not in [self.fields.get(key) for key in ['date', 'time']]:
@@ -14,7 +15,10 @@ class WowzaEntry(logfileparser.DelimitedLogEntry):
             t = datetime.time(*hms)
             #t = datetime.time.strptime(self.fields['time'], '%H:%M:%S')
             self.dt = datetime.datetime.combine(d, t)
-        
+    def get_dict(self):
+        d = super(WowzaEntry, self).get_dict()
+        d['id'] = self.id.strftime(self._datetime_fmt_str)
+        return d
         
 class Session(object):
     def __init__(self, **kwargs):
@@ -32,11 +36,14 @@ class Session(object):
             if e.fields['x-event'] == 'disconnect':
                 self.end_entry = e
                 break
-            self.entries[e.dt] = e
+            self.entries[e.id] = e
+    def get_dict(self):
+        return {'id':self.id, 'entry_ids':self.entries.keys()}
         
 class WowzaLogParser(logfileparser.W3CExtendedLogfileParser):
     entry_class = WowzaEntry
     def __init__(self, **kwargs):
+        kwargs.setdefault('delimiter', 'tab')
         super(WowzaLogParser, self).__init__(**kwargs)
         self.sessions = []
     def parse_file(self, **kwargs):
@@ -50,7 +57,8 @@ class WowzaLogParser(logfileparser.W3CExtendedLogfileParser):
             e = d['entries'][i]
             while e.dt in by_dt:
                 e.dt += td
-            by_dt[e.dt] = e
+            e.id = e.dt
+            by_dt[e.id] = e
         d['entries'] = by_dt
         return d
     def build_sessions(self):
@@ -64,3 +72,9 @@ class WowzaLogParser(logfileparser.W3CExtendedLogfileParser):
             s = Session(start_entry=e, start_index=i, parser=self)
             #d[s.id] = s
             sessions.append(s)
+    def get_dict(self):
+        d = super(WowzaLogParser, self).get_dict()
+        sessions = self.sessions
+        keys = sessions.keys()
+        d['sessions'] = dict(zip(keys, [sessions[key].get_dict() for key in keys]))
+        return d
---
+++ wowzatojson.py
@@ -0,0 +1,89 @@
+import os.path
+import datetime
+import json
+
+from Bases import BaseObject, Config
+from wowzaparser import WowzaEntry, WowzaLogParser
+
+b = BaseObject()
+b.GLOBAL_CONFIG['app_name'] = 'wowza-json'
+
+def datetime_to_string(dt):
+    return dt.strftime(WowzaEntry._datetime_fmt_str)
+def string_to_datetime(s):
+    return datetime.datetime.strptime(WowzaEntry._datetime_fmt_str)
+    
+class WowzaToJson(BaseObject, Config):
+    _confsection = 'LOGFILE_LOCATIONS'
+    def __init__(self, **kwargs):
+        BaseObject.__init__(self, **kwargs)
+        Config.__init__(self, **kwargs)
+        self.log_source = os.path.expanduser(self.get_conf('log_source', '~/wmslogs/source'))
+        self.output_path = os.path.expanduser(self.get_conf('output_path', '~/wmslogs/json'))
+        self.log_names = self.get_conf('log_names', ['access', 'error', 'stats'])
+        for log_name in self.log_names:
+            self.process_log(log_name=log_name)
+    def sort_logfiles(self, **kwargs):
+        log_name = kwargs.get('log_name')
+        base_dir = self.log_source
+        filenames = []
+        fn_by_dt = {}
+        for fn in os.listdir(basedir):
+            if log_name not in fn:
+                continue
+            filenames.append(fn)
+            ts = os.stat(os.path.join(basedir, fn)).st_ctime
+            dt = datetime.datetime.fromtimestamp(ts)
+            fn_by_dt[dt] = fn
+        filenames.sort()
+        filenames.reverse()
+        filenames.remove(log_name)
+        filenames = [log_name] + filenames
+        return filenames, fn_by_dt
+    def process_log(self, **kwargs):
+        existing = self.parse_json(**kwargs)
+        if existing is False:
+            existing = {'entries':{}}
+        dt_list = [string_to_datetime(key) for key in existing['entries'].keys()]
+        last_dt = max(dt_list)
+        filenames, fn_by_dt = self.sort_logfiles(**kwargs)
+        new_fn_dt = sorted(fn_by_dt.keys())
+        for dt in new_fn_dt[:]:
+            if dt < last_dt:
+                new_fn_dt.remove(dt)
+        for dt in new_fn_dt:
+            fn = fn_by_dt[dt]
+            p = WowzaLogParser()
+            p.filename = fn
+            for key, val in p.get_dict().iteritems():
+                if key not in existing:
+                    existing[key] = {}
+                existing[key].update(val)
+        kwargs['data'] = existing
+        self.write_json(**kwargs)
+    def build_filename(self, **kwargs):
+        log_name = kwargs.get('log_name')
+        fn = '%s.json' % (os.path.join(self.output_path, log_name))
+        return fn
+    def parse_json(self, **kwargs):
+        fn = self.build_filename(**kwargs)
+        if not os.path.exists(fn):
+            return False
+        f = open(fn, 'r')
+        d = False
+        try:
+            d = json.load(f)
+        finally:
+            f.close()
+        return d
+    def write_json(self, **kwargs):
+        fn = self.build_filename(**kwargs)
+        data = kwargs.get('data')
+        f = open(fn, 'w')
+        try:
+            json.dump(data)
+        finally:
+            f.close()
+        
+if __name__ == '__main__':
+    wj = WowzaToJson()
